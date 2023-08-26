08fd59ab951b141e5c99ef72833c18da9bddc329
rika kaminoyama <richun.06@gmail.com>
2023-08-26 19:00:01 +0900

    新規投稿画面の作成、 投稿に失敗した場合の設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,20 @@
 class PicturesController < ApplicationController
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    @picture.user_id = current_user.id
+    if @picture.save
+
+    else
+      render :new
+    end
+  end
+
+  def picture_params
+    params.require(:picture).permit(:content, :image, :user_id)
+  end
 end
