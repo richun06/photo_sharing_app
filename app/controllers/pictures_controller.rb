1ee56cc02fbee40f511132a73d8cb38f31d30530
rika kaminoyama <richun.06@gmail.com>
2023-08-26 19:20:53 +0900

    新規投稿が成功した場合の処理の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,9 @@
 class PicturesController < ApplicationController
 
+  def index
+    @pictures = Picture.all
+  end
+
   def new
     @picture = Picture.new
   end
@@ -8,13 +12,13 @@ class PicturesController < ApplicationController
     @picture = Picture.new(picture_params)
     @picture.user_id = current_user.id
     if @picture.save
-
+      redirect_to pictures_path, notice: "投稿を完了しました"
     else
       render :new
     end
   end
 
   def picture_params
-    params.require(:picture).permit(:content, :image, :user_id)
+    params.require(:picture).permit(:content, :image, :user_id, :image_cache)
   end
 end
